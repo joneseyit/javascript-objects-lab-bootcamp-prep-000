7c3b0a94c4edcf4db356e2b6a5b4f67672eb4859
joneseyit <joneseyit@protonmail.com>
2018-10-02 01:35:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   
   return Object.assign(object, {[key]: value})
 }
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, )
+}
